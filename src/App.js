b504c373b78e94eca56ccaaa936c4957ad1a65a8
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2023-01-17 14:56:18 +0700

    update

--- src/App.js
+++ src/App.js
@@ -10,13 +10,43 @@ import { Route, Switch, useHistory } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [posts, setPosts] = useState([
+    {
+      id: 1,
+      title: "My First Post",
+      datetime: "Jan 22, 2001 06:00:00 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 2,
+      title: "My 2nd Post",
+      datetime: "Jan 22, 2001 06:00:00 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 3,
+      title: "My 3rd Post",
+      datetime: "Jan 22, 2001 06:00:00 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    },
+    {
+      id: 4,
+      title: "My Fourth Post",
+      datetime: "Jan 22, 2001 06:00:00 AM",
+      body: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis consequatur expedita, assumenda similique non optio! Modi nesciunt excepturi corrupti atque blanditiis quo nobis, non optio quae possimus illum exercitationem ipsa!"
+    }
+  ])
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
+
   return (
     <div className="App">
-      <Header />
-      <Nav />
+      <Header title="ReactJS Blog" />
+      <Nav search={search} setSearch={setSearch} />
       <Switch>
         <Route exact path="/">
-          <Home />
+          <Home posts={posts} />
         </Route>
         <Route exact path="/post">
           <NewPost />
--- src/Nav.js
+++ src/Nav.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
-const Nav = () => {
+const Nav = ({ search, setSearch }) => {
   return (
-    <nav>
-        <h1>nav</h1>
+    <nav className='Nav'>
+        <form className='searchForm' onSubmit={(e) => e.preventDefault()}>
+          <label htmlFor="search">Search Post</label>
+          <input 
+            type="text"
+            id='search'
+            placeholder='Search Post'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)} 
+          />
+        </form>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/post">Post</Link></li>
+        </ul>
     </nav>
   )
 }
