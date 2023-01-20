d5afc906482d4e671e7fb6e080fd5dc692df8d06
ThanapawnTony <113745605+ThanapawnTony@users.noreply.github.com>
2023-01-20 08:22:16 +0700

    submit button

--- src/NewPost.js
+++ src/NewPost.js
@@ -9,7 +9,11 @@ const NewPost = ({ handleSubmit, setPostTitle, postTitle, postBody, setPostBody 
           <input type="text" id='postTitle' required value={postTitle} onChange={(e) => setPostTitle(e.target.value)} />
           <label htmlFor="postBody">Post:</label>
           <textarea name="" id="postBody" required value={postBody} onChange={(e) => setPostBody(e.target.value)} />
-          <button type='submit'>Submit</button>
+          <button type='submit'>
+            <span>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"><path fill="none" d="M0 0h24v24H0z"></path><path fill="currentColor" d="M11 11V5h2v6h6v2h-6v6h-2v-6H5v-2z"></path></svg> Submit
+            </span>
+          </button>
         </form>
     </main>
   )
